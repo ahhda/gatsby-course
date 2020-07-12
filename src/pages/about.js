43b36ed923996fbfda45c4c23b9a7f64004791ea
Anuj Bansal <bansalanuj1996@gmail.com>
2020-07-12 15:14:55 +0530

    Add styling and layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function About() {
   return (
-    <div>
-      <Link to="/">Home</Link>
+    <Layout>
       <h1>About</h1>
       <p>This is the about page.</p>
-    </div>
+    </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div>
-      <Link to="/about/">About</Link>
-      <div>Hello Gatsby!</div>
-    </div>
+    <Layout>
+      <h1>Welcome to my Blog!</h1>
+      <p>This is where I post all the cool stuff.</p>
+    </Layout>
   )
 }
